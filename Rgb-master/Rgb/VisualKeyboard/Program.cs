1fb45a80b4d06bb5d825e8cb4d257b451628e00c
Chris <christopher.yazbek@gmail.com>
2016-11-14 21:22:06 -0500

    TMT - Corsair Hack k70 - Yhack

---
+++ Rgb-master/Rgb/VisualKeyboard/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HidLibrary;
+using RgbHidLibrary;
+
+namespace VisualKeyboard
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            RgbHidBase rgbDevice;
+            RgbHid.TryCreate(RgbDeviceType.CorsairK70Rgb, out rgbDevice);
+            K70Rgb myKeyboard = rgbDevice as K70Rgb;
+
+
+            IDictionary<uint, Color> x = new Dictionary<uint, Color>();
+            x.Add(7, Color.Red);
+            myKeyboard.DrawKeys(x);
+        }
+    }
+}
